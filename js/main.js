c3a00534f1ac72e3efdceaa722d9d6fdb259656e
SalasDimasCarlos <carlos.salas@alumnos.udg.mx>
2018-03-15 08:35:59 -0600

    Actions timer added to INDEX.HTML and ACTIOS JS

--- js/main.js
+++ js/main.js
@@ -2,4 +2,15 @@ var a = 5;  //Local al bloque
 let b = 10; // Local a la funcion
 x = 42; // Global
 
-const
+const B = ; //Constante con alcance local a la funcion
+
+array = [
+  1,
+  2,
+  3
+];
+
+objeto  = {
+  atr1: 'cadena',
+  atr2: 5
+};
